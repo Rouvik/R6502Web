93d92ae30c32e5dfca2d2359b021b39bc1988a91
Rouvik Maji <majirouvik@gmail.com>
2025-05-26 02:39:35 +0530

    Improved and updated helper.hpp for nesspecific logging requirements

--- include/helper.hpp
+++ include/helper.hpp
@@ -3,8 +3,18 @@
 
 #include <iostream>
 
+// ####################### toggle casserts #######################
+// #define NDEBUG
+
+#include <cassert>
+
 #define LOG_INFO << __FILE__ ":" << __LINE__ << " (" << __FUNCTION__ << ") -- " <<
 // #define LOG_INFO << // Uncomment this line to disable detailed logging
+
+// ####################### toggle project debug features on/off #######################
+#define ENABLE_DEBUG
+
+#ifdef ENABLE_DEBUG
 
 #define LOG(TAG, msg)                                        \
     do                                                       \
@@ -23,5 +33,15 @@
     {                                                         \
         std::cerr << "[WARN " #TAG "] " LOG_INFO msg << '\n'; \
     } while (0)
+
+#else
+
+#define LOG(TAG, msg) ((void)0)
+
+#define ERROR(TAG, msg) ((void)0)
+
+#define WARN(TAG, msg) ((void)0)
+
+#endif
 
 #endif // INCLUDED_HELPER_HPP
