21d517e2ce05dd85322971fe0d6dd6bea3157bf9
Rouvik Maji <majirouvik@gmail.com>
2025-10-02 05:05:44 +0530

    UI update almost done, left with final polishes and credits addition

--- main.mjs
+++ main.mjs
@@ -2,21 +2,30 @@ import r6502Module from "./bin/r6502.mjs";
 import asm6Module from "./bin/asm6.mjs";
 import MemoryContainer from "./lib/memory_container.mjs";
 
-window.addEventListener('load', main);
-
+// global state
 let r6502Mod = null;
 let r6502CPUPtr = null;
-
 let asm6Mod = null;
 let intervalIndex = null;
-
 let editor = null;
-
 const memContainer = new MemoryContainer(document.getElementById("memory-container"));
-
 const cpuState = document.getElementById("cpuState");
-
 const programInput = document.getElementById("programInput");
+const romStartOffset = document.getElementById("romStartOffset");
+
+const cpuStateReg = {
+    ip: document.getElementById("ipVal"),
+    stack: document.getElementById("stackVal"),
+    xreg: document.getElementById("regXVal"),
+    yreg: document.getElementById("regYVal"),
+    areg: document.getElementById("regAVal"),
+    status: document.getElementById("statusVal")
+};
+
+// main function as starting point
+window.addEventListener('load', main);
+
+// program ROM input handler
 programInput.addEventListener("input", () => {
     const fr = new FileReader();
     fr.readAsArrayBuffer(programInput.files[0]);
@@ -24,41 +33,51 @@ programInput.addEventListener("input", () => {
     fr.onload = () => {
         const memPtr = r6502Mod.getMemoryPtr(r6502CPUPtr);
         const heapArr = r6502Mod.HEAPU8.subarray(memPtr, memPtr + 0x10000);
-        heapArr.set(new Uint8Array(fr.result), 0x600);
 
-        r6502Mod.R6502.IP = 0x600;
-        heapArr[0xFFFA] = 0x11;
-        heapArr[0xFFFB] = 0x06;
-        heapArr[0xFFFE] = 0x0A;
-        heapArr[0xFFFF] = 0x06;
-
-        memContainer.buffer = heapArr;
-        memContainer.refreshArrayView();
+        setROMToMemory(fr.result, parseInt(romStartOffset.value, 16));
+        r6502Mod.R6502.IP = heapArr[0xfffc] | (heapArr[0xfffd] << 8);
         printCPUState();
     }
 });
 
+function setROMToMemory(rom, offset = 0) {
+    const memPtr = r6502Mod.getMemoryPtr(r6502CPUPtr);
+    const heapArr = r6502Mod.HEAPU8.subarray(memPtr, memPtr + 0x10000);
+    heapArr.set(new Uint8Array(rom), offset);
+
+    memContainer.buffer = heapArr;
+    memContainer.refreshArrayView();
+}
+
+// CPU single step button handler
 document.getElementById("clockBtn").addEventListener("click", () => {
     r6502Mod.clockCPUFromJS(r6502CPUPtr);
     memContainer.refreshArrayView();
     printCPUState();
 });
 
+// Interrupt Button Handler
 document.getElementById("irqBtn").addEventListener("click", () => {
     r6502Mod.callIRQFromJS(r6502CPUPtr);
 });
 
+// Non Maskable Interrupt Button Handler
 document.getElementById("nmiBtn").addEventListener("click", () => {
     r6502Mod.callNMIFromJS(r6502CPUPtr);
 });
 
+// CPU Reset button handler
 document.getElementById("resetBtn").addEventListener("click", () => {
+    const memPtr = r6502Mod.getMemoryPtr(r6502CPUPtr);
+    const heapArr = r6502Mod.HEAPU8.subarray(memPtr, memPtr + 0x10000);
+
     r6502Mod.resetCPUFromJS(r6502CPUPtr);
-    r6502Mod.R6502.IP = 0x600;
+    r6502Mod.R6502.IP = heapArr[0xfffc] | (heapArr[0xfffd] << 8);
     r6502Mod.R6502.running = true;
     printCPUState();
 });
 
+// CPU run button handler
 document.getElementById("runBtn").addEventListener("click", () => {
     if (intervalIndex) {
         return;
@@ -71,6 +90,7 @@ document.getElementById("runBtn").addEventListener("click", () => {
     }, 50);
 });
 
+// CPU Stop button handler
 document.getElementById("stopBtn").addEventListener("click", () => {
     if (!!intervalIndex) {
         clearInterval(intervalIndex);
@@ -83,16 +103,43 @@ function printCPUState() {
     cpuState.value = 
 `Total Cycles: ${r6502Mod.R6502.total_cycles}
 Ticks: ${r6502Mod.R6502.ticks}
-IP: 0x${r6502Mod.R6502.IP.toString(16)} (${r6502Mod.R6502.IP})
 Instruction: ${r6502Mod.getInstructionName(r6502Mod.R6502.instr)}
-Running: ${r6502Mod.R6502.running}
-Stack: 0x${r6502Mod.R6502.reg_Stack.toString(16)} (${r6502Mod.R6502.reg_Stack})
-X register: ${r6502Mod.R6502.reg_X}
-Y register: ${r6502Mod.R6502.reg_Y}
-A register (Accumulator): ${r6502Mod.R6502.reg_Acc}
-Status register: ${r6502Mod.R6502.reg_Status.toString(2).padStart(8, '0')}`;
+Running: ${r6502Mod.R6502.running}`;
+
+    cpuStateReg.ip.value = `0x${r6502Mod.R6502.IP.toString(16)}`;
+    cpuStateReg.stack.value = `0x${r6502Mod.R6502.reg_Stack.toString(16)}`;
+    cpuStateReg.xreg.value = r6502Mod.R6502.reg_X;
+    cpuStateReg.yreg.value = r6502Mod.R6502.reg_Y;
+    cpuStateReg.areg.value = r6502Mod.R6502.reg_Acc;
+    cpuStateReg.status.value = r6502Mod.R6502.reg_Status.toString(2).padStart(8, '0');
 }
 
+// listeners for CPU State
+cpuStateReg.ip.addEventListener("input", () => {
+    r6502Mod.R6502.IP = parseInt(cpuStateReg.ip.value, 16) || 0;
+});
+
+cpuStateReg.stack.addEventListener("input", () => {
+    r6502Mod.R6502.reg_Stack = parseInt(cpuStateReg.stack.value, 16) || 0;
+});
+
+cpuStateReg.xreg.addEventListener("input", () => {
+    r6502Mod.R6502.reg_X = +cpuStateReg.xreg.value || 0;
+});
+
+cpuStateReg.yreg.addEventListener("input", () => {
+    r6502Mod.R6502.reg_Y = +cpuStateReg.yreg.value || 0;
+});
+
+cpuStateReg.areg.addEventListener("input", () => {
+    r6502Mod.R6502.reg_Acc = +cpuStateReg.areg.value || 0;
+});
+
+cpuStateReg.status.addEventListener("input", () => {
+    r6502Mod.R6502.reg_Status = parseInt(cpuStateReg.status.value, 2) || 0;
+});
+
+// assemble and copy code to buffer button handler
 document.getElementById("codeCompileBtn").addEventListener("click", () => {
     asm6Mod.FS.writeFile("prog.c", editor.getValue());
     if(asm6Mod.callMain(["prog.c", "prog.bin"]))
@@ -103,19 +150,13 @@ document.getElementById("codeCompileBtn").addEventListener("click", () => {
 
     const memPtr = r6502Mod.getMemoryPtr(r6502CPUPtr);
     const heapArr = r6502Mod.HEAPU8.subarray(memPtr, memPtr + 0x10000);
-    heapArr.set(new Uint8Array(asm6Mod.FS.readFile("prog.bin")), 0x600);
 
-    r6502Mod.R6502.IP = 0x600;
-    heapArr[0xFFFA] = 0x11;
-    heapArr[0xFFFB] = 0x06;
-    heapArr[0xFFFE] = 0x0A;
-    heapArr[0xFFFF] = 0x06;
-
-    memContainer.buffer = heapArr;
-    memContainer.refreshArrayView();
+    setROMToMemory(asm6Mod.FS.readFile("prog.bin"), parseInt(romStartOffset.value, 16));
+    r6502Mod.R6502.IP = heapArr[0xfffc] | (heapArr[0xfffd] << 8);
     printCPUState();
 });
 
+// main driver code
 async function main() {
     r6502Mod = await r6502Module();
     r6502CPUPtr = r6502Mod.initialiseCPU();
@@ -123,5 +164,5 @@ async function main() {
         
     editor = ace.edit("editor");
     editor.setTheme("ace/theme/dracula");
-    editor.session.setMode("ace/mode/assembly_6502");    
+    editor.session.setMode("ace/mode/assembly_6502");
 }
